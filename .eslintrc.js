e913275e88693cb556539dee9ec379ec1bd80109
niedermansam <niederman.sam@gmail.com>
2022-10-08 20:01:43 -0600

    deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'max-len': 0,
+    'linebreak-style': 0,
   },
+  ignorePatterns: ['dist/*'],
 };
