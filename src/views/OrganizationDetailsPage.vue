35c199641cf7bf61c99b03b80d626490c90a9c48
niedermansam <niederman.sam@gmail.com>
2022-10-17 12:29:13 -0600

    add organization details

--- src/views/OrganizationDetailsPage.vue
+++ src/views/OrganizationDetailsPage.vue
@@ -1,55 +1,121 @@
-<template>
-  <div class="organization">
-    <h1>{{ organization.Name }}</h1>
-
-    <span
-      class="badge rounded-pill"
-      :style="{ backgroundColor: chipStyles(organization.Expertise).color }"
-    >
-      {{ chipStyles(organization.Expertise).emoji }}
-      {{ organization.Expertise }}
-    </span>
-
-    <p class="card-text">
-      {{ organization.Notes }}
-    </p>
-  </div>
-</template>
-
 <script lang="ts">
 import { categoryChipStyles } from "../hooks/categoryHooks";
-import { useOrganizationStore } from "../store/index";
-import { defineComponent } from "vue";
+import { useOrganizationStore, useResourceStore } from "../store/index";
+import { defineComponent, ref, watch } from "vue";
 import { useRoute } from "vue-router";
+import type { ResourceSchema } from "@/schemas";
+import { ResourceCard } from "@/components";
+import { onlyUnique } from "@/hooks";
+import TagLinks from "../components/TagLinks.vue";
 
 export default defineComponent({
   props: {
     id: { type: String },
   },
+  components: {
+    ResourceCard,
+    TagLinks
+},
   name: "OrganizationDetails",
 
   setup(props) {
     const route = useRoute();
-    const currentId = route.params.id as string;
+    const currentId = ref(route.params.id as string);
+
+    function updateRoute(){
+      currentId.value = route.params.id as string;
+      organization.value = organizationStore.lookup[currentId.value];
+    }
+
+    watch(() => route.params.id, updateRoute)
 
     const chipStyles = categoryChipStyles;
 
     const organizationStore = useOrganizationStore();
+    const resourceStore = useResourceStore();
 
     async function loadAllData() {
       await organizationStore.loadData();
+      await resourceStore.loadData();
+      organization.value = organizationStore.lookup[currentId.value];
+
+      /* TODO: clean up this shit
+      Gets a list of resources that the organization can help with, but does not administer */
+      resourceList.value = organization.value['Resources Available'] ? 
+      organization.value['Resources Available']
+        .map((id:string) => resourceStore.lookup[id]) 
+        .filter(((resource:ResourceSchema) => organization.value['Administer of'] ? organization.value['Administer of'].indexOf(resource.id) === -1 : true))
+        : [];
+      
+      administersList.value = organization.value['Administer of'] ? organization.value['Administer of'].map((id:string) => resourceStore.lookup[id]) : [];
+
+      tagList.value = organization.value['Resources Available'] ? organization.value['Resources Available'].map((resourceId:string) => {
+        const currentResource = resourceStore.lookup[resourceId]
+        return currentResource.Tags
+      }).flat().filter(onlyUnique) : [];
+
     }
     loadAllData();
 
-    const organization = organizationStore.lookup[currentId];
+    const organization = ref(organizationStore.lookup[currentId.value]);
+    const resourceList = ref<ResourceSchema[]>([]);
+    const administersList = ref<ResourceSchema[]>([]);
+    const tagList = ref<string[]>([])
 
     return {
       route,
       currentId,
       organizationStore,
       organization,
+      administersList,
+      resourceList,
+      tagList,
       chipStyles,
     };
   },
 });
 </script>
+
+<template>
+  <div v-if="organization" class="organization">
+    <h1>{{ organization.Name }}</h1>
+    <p v-if="organization.Phone">phone: {{organization.Phone}}</p>
+    <p v-if="organization.Email">email: {{organization.Email}}</p>
+    <p v-if="organization.URL">website: <a :href="organization.URL" target="_blank"> {{organization.URL}}</a></p>
+
+    <div v-if="tagList.length > 0">
+    <p>Resource Tags:</p>
+    <TagLinks :tagArr="tagList"/>
+    </div>
+
+    <p class="card-text">
+      {{ organization.Notes }}
+    </p>
+
+    <div v-if="administersList.length > 0">
+    <h2>{{organization.Name}} offers the following resources:</h2>
+    <div style="display:flex; flex-wrap: wrap;">
+    <ResourceCard
+          v-for="resource in administersList"
+          style="margin: 5px"
+          :key="resource.id"
+          :resource="resource"
+        />
+    </div>
+  </div>
+    <div v-if="resourceList.length > 0">
+    <h2>{{organization.Name}} can help with:</h2>
+    <div style="display:flex; flex-wrap: wrap;">
+    <ResourceCard
+          v-for="resource in resourceList"
+          style="margin: 5px"
+          :key="resource.id"
+          :resource="resource"
+        />
+    </div>
+  </div>
+
+  <RouterLink to="/organizations/"><button class="btn btn-primary">See More Organizations</button></RouterLink>
+
+  </div>
+</template>
